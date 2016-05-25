d1611ec307e2b32f5bdbf9b4440ab563e43f3718
Eddy Harless <eharless@gmail.com>
2016-05-25 02:07:53 +0000

    Styled form and homepage

--- app/controllers/concerns/articles_controller.rb
+++ app/controllers/concerns/articles_controller.rb
@@ -19,7 +19,7 @@ class ArticlesController < ApplicationController
 
     if @article.save
 
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
 
       redirect_to article_path(@article)
 
@@ -36,7 +36,7 @@ class ArticlesController < ApplicationController
 # ----- /edit
   def update
     if @article.update(article_params)
-      flash[:notice] = "Article was successfully created"
+      flash[:success] = "Article was successfully created"
       redirect_to article_path(@article)
     else
       render 'edit'
@@ -49,7 +49,7 @@ class ArticlesController < ApplicationController
 # ----- /show
   def destroy
     @article.destroy
-    flash[:notice] = "Article was successfully deleted"
+    flash[:success] = "Article was successfully deleted"
     redirect_to articles_path
 
   end
